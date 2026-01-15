3a8526974b4a8db842430d7a7f1e7d2b49285a0c
Yshvrd <yshvrd@gmail.com>
2026-01-16 02:17:17 +0530

    fix model name

--- Backend/db/models.py
+++ Backend/db/models.py
@@ -7,7 +7,7 @@ Base = declarative_base() # ORM base model
 
 
 # Create table
-class Employees(Base):
+class Employee(Base):
     __tablename__ = "employees"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
