a70fd827ede971fae024067a8dd3506920e3d553
Yshvrd <yshvrd@gmail.com>
2026-01-16 01:07:53 +0530

    Add get employee logic

--- Backend/core/employee_crud.py
+++ Backend/core/employee_crud.py
@@ -29,4 +29,30 @@ def create_employee(db: Session, employee_in: CreateEmployee) -> Employee:
 
 
 # get employee by id 
+def get_employee_by_id(db: Session, employee_id: int) -> Employee | None:
+    result = db.query(Employee).filter(Employee.id == employee_id).first()
+    
+    return result 
+
+# get all employees and pagination logic
+def get_employees(db: Session, page: int = 1, department: str | None = None, role: str | None = None,):
+    query = db.query(Employee)
+
+    # search by department
+    if department:
+        query = query.filter(Employee.department == department)
+    # search by role
+    if role:
+        query = query.filter(Employee.role == role)
+
+    limit = 10 # max per page
+    offset = (page - 1) * limit # page logic (no. of db rows to skip)
+    result = query.offset(offset).limit(limit).all()
+
+    return result 
+
+
+
+
+# Update employee
 
